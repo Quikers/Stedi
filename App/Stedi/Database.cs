17d8d8a0caa12e0f4bcc98736364dfaea0ca2dec
Luuk Diederik <flipmil3@hotmail.com>
2016-09-12 07:40:43 +0200

    App update

---
+++ App/Stedi/Database.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Stedi {
+    static class Database {
+        private static MySqlConnection MySqlConn;
+
+        static Database() {
+            // Init
+            // Create MySQL connection
+            string MySqlConfig = "Server=localhost;Database=stedi;Uid=root;";
+            try {
+                MySqlConn = new MySqlConnection(MySqlConfig);
+                MySqlConn.Open();
+            } catch {
+                // Couldn't connect to database. Show error message and close application
+                MessageBox.Show("Couldn't connect to database.");
+                Environment.Exit(0);
+            }
+        }
+
+        public static List<string[]> query(string sql) {
+            List<string[]> items = new List<string[]>();
+
+            MySqlCommand cmd = new MySqlCommand(sql, MySqlConn);
+            cmd.CommandType = System.Data.CommandType.Text;
+
+            try {
+                // Execute the query and save the result in the game list
+                using (MySqlDataReader reader = cmd.ExecuteReader()) {
+                    while (reader.Read()) {
+                        // Add a new string array
+                        items.Add(new string[reader.FieldCount]);
+
+                        // Set the values of the string array
+                        for (int i = 0; i < reader.FieldCount; i++) {
+                            items[items.Count - 1][i] = reader.GetString(i);
+                        }
+                    }
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("Something went wrong with the database connection.\nStats for nerds:\n\n" + ex);
+            }
+
+            return items;
+        }
+    }
+}
